50c2d97a7835e3830d3f265ec711ed224d028768
冉娃娃· <274544338@qq.com>
2020-01-12 23:28:53 +0800

    btn要干完了

--- packages/mixins/button.ts
+++ packages/mixins/button.ts
@@ -2,18 +2,18 @@ export const button = Behavior({
   externalClasses: ['hover-class'],
 
   properties: {
-    id: String,
-    lang: {
-      type: String,
-      value: 'en'
-    },
+    // id: String,
+    // lang: {
+    //   type: String,
+    //   value: 'en'
+    // },
     businessId: Number,
-    sessionFrom: String,
-    sendMessageTitle: String,
-    sendMessagePath: String,
-    sendMessageImg: String,
-    showMessageCard: Boolean,
-    appParameter: String,
+    // sessionFrom: String,
+    // // sendMessageTitle: String,
+    // // sendMessagePath: String,
+    // // sendMessageImg: String,
+    // // showMessageCard: Boolean,
+    // // appParameter: String,
     ariaLabel: String
   }
 });
--- packages/mixins/open-type.ts
+++ packages/mixins/open-type.ts
@@ -1,33 +1,33 @@
 import { Weapp } from 'definitions/weapp';
 
-export const openType = Behavior({
-  properties: {
-    openType: String
-  },
+// export const openType = Behavior({
+  // properties: {
+    // openType: String
+  // },
 
-  methods: {
-    bindGetUserInfo(event: Partial<Weapp.Event>) {
-      this.$emit('getuserinfo', event.detail);
-    },
+  // methods: {
+    // bindGetUserInfo(event: Partial<Weapp.Event>) {
+    //   this.$emit('getuserinfo', event.detail);
+    // },
 
-    bindContact(event: Partial<Weapp.Event>) {
-      this.$emit('contact', event.detail);
-    },
+    // bindContact(event: Partial<Weapp.Event>) {
+    //   this.$emit('contact', event.detail);
+    // },
 
-    bindGetPhoneNumber(event: Partial<Weapp.Event>) {
-      this.$emit('getphonenumber', event.detail);
-    },
+    // bindGetPhoneNumber(event: Partial<Weapp.Event>) {
+    //   this.$emit('getphonenumber', event.detail);
+    // },
 
-    bindError(event: Partial<Weapp.Event>) {
-      this.$emit('error', event.detail);
-    },
+    // bindError(event: Partial<Weapp.Event>) {
+    //   this.$emit('error', event.detail);
+    // },
 
-    bindLaunchApp(event: Partial<Weapp.Event>) {
-      this.$emit('launchapp', event.detail);
-    },
+    // bindLaunchApp(event: Partial<Weapp.Event>) {
+    //   this.$emit('launchapp', event.detail);
+    // },
 
-    bindOpenSetting(event: Partial<Weapp.Event>) {
-      this.$emit('opensetting', event.detail);
-    },
-  }
-});
+    // bindOpenSetting(event: Partial<Weapp.Event>) {
+    //   this.$emit('opensetting', event.detail);
+    // },
+  // }
+// });
